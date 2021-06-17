08682d95634e4f31da5877528ad8cebaf1bbca15
janismozgis-rcs <35929587+janismozgis-rcs@users.noreply.github.com>
2021-06-17 20:44:59 +0300

    add labels

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
 import taskRoutes from './routes/tasks.js';
+import labelRoutes from './routes/labels.js';
 
 dotenv.config();
 
@@ -12,12 +13,8 @@ const server = express();
 server.use(bodyParser.json());
 
 server.use('/tasks', taskRoutes);
+server.use('/labels', labelRoutes);
 
-
-// GET /labels - returns list of all the labels
-// POST /labels - create a new label. {"title": "foo", "color": "#ff0000"}
-// PUT /labels/{id} - update the label {"title": "foo", "color": "#ff0000"}
-// DELETE /labels/{id} - deletes a label
 
 mongoose.connect(
     process.env.MONGOOSE_CONNECTION_URL, 
